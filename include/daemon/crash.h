bdf2128a81a566bf731b4bdc857e41d0c5d280ca
seclususs <antoa7767@gmail.com>
2026-06-24 04:50:38 +0700

    Add fatal signal crash handlers for OS protection

---
+++ include/daemon/crash.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-only
+// Copyright (C) 2026 seclususs
+
+#ifndef PASCAL_GOV_DAEMON_CRASH_H
+#define PASCAL_GOV_DAEMON_CRASH_H
+
+void pascal_gov_crash_init(void);
+
+#endif // PASCAL_GOV_DAEMON_CRASH_H
