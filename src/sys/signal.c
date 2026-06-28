2c9260b7bc43ffa5424e5e53a28374ca166eb1f9
seclususs <antoa7767@gmail.com>
2026-06-28 19:47:42 +0700

    Merge crash into signal and apply PASCAL_GOV_UNUSED

--- src/sys/signal.c
+++ src/sys/signal.c
@@ -3,10 +3,24 @@
 
 #include "daemon/signal.h"
 #include "daemon/logger.h"
+#include "pascal_gov/compiler.h"
 #include <errno.h>
 #include <signal.h>
 #include <sys/signalfd.h>
 #include <unistd.h>
+
+static void pascal_gov_crash_handler(int sig, siginfo_t *info, void *context)
+{
+	PASCAL_GOV_UNUSED(info);
+	PASCAL_GOV_UNUSED(context);
+
+	const char msg[] = "\n[PASCAL-GOV] fatal: daemon crashed\n";
+	ssize_t unused = write(STDERR_FILENO, msg, sizeof(msg) - 1);
+	PASCAL_GOV_UNUSED(unused);
+
+	(void)signal(sig, SIG_DFL);
+	(void)raise(sig);
+}
 
 int pascal_gov_signal_init(void)
 {
@@ -33,9 +47,21 @@ int pascal_gov_signal_init(void)
 	return sfd;
 }
 
+void pascal_gov_signal_crash(void)
+{
+	struct sigaction sa = {0};
+	sa.sa_flags = SA_SIGINFO | SA_RESTART;
+	sa.sa_sigaction = pascal_gov_crash_handler;
+	sigemptyset(&sa.sa_mask);
+
+	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGFPE, &sa, NULL);
+	sigaction(SIGABRT, &sa, NULL);
+	sigaction(SIGILL, &sa, NULL);
+}
+
 void pascal_gov_signal_destroy(int fd)
 {
-	if (fd >= 0) {
+	if (fd >= 0)
 		close(fd);
-	}
 }
