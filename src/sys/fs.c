e04fe34512294140e39922873357d0777b9bc772
seclususs <antoa7767@gmail.com>
2026-07-13 19:54:00 +0700

    Extract dirent processing from walk into a helper

--- src/sys/fs.c
+++ src/sys/fs.c
@@ -14,6 +14,57 @@
 
 #define DIR_FLAGS (O_RDONLY | O_DIRECTORY | O_NOFOLLOW | O_CLOEXEC | O_NOATIME)
 
+static int walk(int dfd, pg_fs_cb cb, void *ctx, int max_depth, int cur_depth);
+
+static int dirent(int dfd, struct linux_dirent64 *d, pg_fs_cb cb, void *ctx,
+		  int max_depth, int cur_depth)
+{
+	uint8_t dtype = d->d_type;
+
+	if (d->d_name[0] == '.' &&
+	    (d->d_name[1] == '\0' ||
+	     (d->d_name[1] == '.' && d->d_name[2] == '\0')))
+		return 0;
+
+	if (dtype == DT_LNK)
+		return 0;
+
+	if (UNLIKELY(dtype == DT_UNKNOWN)) {
+		struct stat st;
+		int ret;
+
+		ret = fstatat(dfd, d->d_name, &st, AT_SYMLINK_NOFOLLOW);
+		if (ret == 0 && S_ISDIR(st.st_mode))
+			dtype = DT_DIR;
+	}
+
+	enum pg_fs_act act = cb(dfd, d->d_name, dtype, cur_depth, ctx);
+
+	if (act == FS_STOP)
+		return 1;
+
+	if (act == FS_DELETE) {
+		int flgs = (dtype == DT_DIR) ? AT_REMOVEDIR : 0;
+		unlinkat(dfd, d->d_name, flgs);
+		return 0;
+	}
+
+	if (dtype == DT_DIR) {
+		int sfd = openat(dfd, d->d_name, DIR_FLAGS);
+		if (sfd < 0)
+			return 0;
+
+		int stop = walk(sfd, cb, ctx, max_depth, cur_depth + 1);
+
+		close(sfd);
+
+		if (stop)
+			return stop;
+	}
+
+	return 0;
+}
+
 static int walk(int dfd, pg_fs_cb cb, void *ctx, int max_depth, int cur_depth)
 {
 	char buf[8192];
@@ -27,52 +78,10 @@ static int walk(int dfd, pg_fs_cb cb, void *ctx, int max_depth, int cur_depth)
 
 		while (bpos < nread) {
 			struct linux_dirent64 *d = (void *)(buf + bpos);
-			uint8_t dtype = d->d_type;
-
 			bpos += d->d_reclen;
 
-			if (d->d_name[0] == '.') {
-				if (d->d_name[1] == '\0' ||
-				    (d->d_name[1] == '.' &&
-				     d->d_name[2] == '\0'))
-					continue;
-			}
-
-			if (dtype == DT_LNK)
-				continue;
-
-			if (UNLIKELY(dtype == DT_UNKNOWN)) {
-				struct stat st;
-				if (fstatat(dfd, d->d_name, &st,
-					    AT_SYMLINK_NOFOLLOW) == 0 &&
-				    S_ISDIR(st.st_mode))
-					dtype = DT_DIR;
-			}
-
-			enum pg_fs_act act =
-				cb(dfd, d->d_name, dtype, cur_depth, ctx);
-
-			if (act == FS_STOP)
+			if (dirent(dfd, d, cb, ctx, max_depth, cur_depth))
 				return 1;
-
-			if (act == FS_DELETE) {
-				int flgs = (dtype == DT_DIR) ? AT_REMOVEDIR : 0;
-				unlinkat(dfd, d->d_name, flgs);
-				continue;
-			}
-
-			if (dtype == DT_DIR) {
-				int sfd = openat(dfd, d->d_name, DIR_FLAGS);
-				if (sfd < 0)
-					continue;
-
-				int stop = walk(sfd, cb, ctx, max_depth,
-						cur_depth + 1);
-				close(sfd);
-
-				if (stop)
-					return stop;
-			}
 		}
 	}
 
