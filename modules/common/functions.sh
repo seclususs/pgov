5042460b74b44aa88443386bb90ffc57ecebfc6d
seclususs <antoa7767@gmail.com>
2026-07-20 06:41:03 +0700

    Add pgovd.conf and update module installer scripts

--- modules/common/functions.sh
+++ modules/common/functions.sh
@@ -1,5 +1,23 @@
 #!/system/bin/sh
 
+print_header() {
+    local mod_id=$(sed -n "s/^id=//p" "$MODPATH/module.prop")
+    local mod_name=$(sed -n "s/^name=//p" "$MODPATH/module.prop")
+    local mod_version=$(sed -n "s/^version=//p" "$MODPATH/module.prop")
+    local mod_versioncode=$(sed -n "s/^versionCode=//p" "$MODPATH/module.prop")
+    local mod_author=$(sed -n "s/^author=//p" "$MODPATH/module.prop")
+    local mod_description=$(sed -n "s/^description=//p" "$MODPATH/module.prop")
+    
+    ui_print "*******************************************"
+    ui_print "  ID           : $mod_id"
+    ui_print "  Name         : $mod_name"
+    ui_print "  Version      : $mod_version"
+    ui_print "  Version Code : $mod_versioncode"
+    ui_print "  Author       : $mod_author"
+    ui_print "  Description  : $mod_description"
+    ui_print "*******************************************"
+}
+
 extract_binary() {
     mkdir -p "$MODPATH/system/bin"
     
@@ -10,6 +28,10 @@ extract_binary() {
     fi
 }
 
+extract_conf() {
+    unzip -o "$ZIPFILE" 'system/etc/pgovd.conf' -d "$MODPATH" >&2
+}
+
 extract_services() {
     unzip -o "$ZIPFILE" 'service.sh' 'uninstall.sh' -d "$MODPATH" >&2
 }
@@ -17,6 +39,7 @@ extract_services() {
 set_permissions() {
     set_perm_recursive "$MODPATH" 0 0 0755 0644
     set_perm "$MODPATH/system/bin/pgovd" 0 0 0755 u:object_r:system_file:s0
+    set_perm "$MODPATH/system/etc/pgovd.conf" 0 0 0644
     set_perm "$MODPATH/service.sh" 0 0 0755 u:object_r:script_exec:s0
     set_perm "$MODPATH/uninstall.sh" 0 0 0755
 }
--- modules/common/install.sh
+++ modules/common/install.sh
@@ -2,8 +2,13 @@
 
 . "$MODPATH/common/functions.sh"
 
+print_header
+
 ui_print "- Extracting binaries..."
 extract_binary
+
+ui_print "- Extracting conf..."
+extract_conf
 
 ui_print "- Configuring service..."
 extract_services
