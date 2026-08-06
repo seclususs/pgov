eed80310c1f4ae4113a336a0e641cfe1b06a0571
seclususs <antoa7767@gmail.com>
2026-08-06 11:49:43 +0700

    Standardize NDK_BUILD macro scope in sweep header

--- include/pg/sweep.h
+++ include/pg/sweep.h
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-only
 // Copyright (C) 2026 seclususs
 
+#if defined(NDK_BUILD)
+
 #ifndef PG_SWEEP_H
 #define PG_SWEEP_H
 
 #include "pg/state.h"
 
-#if defined(NDK_BUILD)
 void pg_sweep_run(struct pg_context *ctx);
-#endif // NDK_BUILD
 
 #endif // PG_SWEEP_H
+
+#endif // NDK_BUILD
